d84131d219b8b29253227884441e52dfa5322d52
Matsuko Friedland <info@matsuko.ca>
2019-05-06 20:51:10 -0700

    Rename solution variable in test.js
    Was confusing it with solutions variable in index.js

--- 2019-05/test.js
+++ 2019-05/test.js
@@ -12,61 +12,61 @@
   - Variables can be overwritten/changed before running tests
 */
 const rewire = require('rewire');
-const solution = rewire('./index');
+const code = rewire('./index');
 
 /** TESTS **/
 
 // Challenge #1
 test('Challenge #1: `powerOn` function', () => {
-  solution.__get__('powerOn')();
-  expect(solution.__get__('ship').powerOn).toBe(true);
+  code.__get__('powerOn')();
+  expect(code.__get__('ship').powerOn).toBe(true);
 });
 
 // Challenge #2
 test('Challenge #2: `countModules` function', () => {
-  expect(solution.__get__('countModules')()).toBe(4);
+  expect(code.__get__('countModules')()).toBe(4);
 });
 
 // Challenge #3
 test('Challenge #3: `countEssential` function', () => {
-  expect(solution.__get__('countEssential')()).toBe(2);
+  expect(code.__get__('countEssential')()).toBe(2);
 });
 
 // Challenge #4
 test('Challenge #4: `loadModule` function', () => {
-  const shipModules = solution.__get__('ship').modules;
-  const availableModules = solution.__get__('availableModules');
-  solution.__get__('loadModule')(3);
+  const shipModules = code.__get__('ship').modules;
+  const availableModules = code.__get__('availableModules');
+  code.__get__('loadModule')(3);
   expect(shipModules[shipModules.length - 1]).toEqual(availableModules[3]);
 });
 
 test('Challenge #4: `life-support` module is enabled', () => {
-  expect(solution.__get__('availableModules')[1].enabled).toBe(true);
+  expect(code.__get__('availableModules')[1].enabled).toBe(true);
 });
 
 test('Challenge #4: life-support` module added to shipModules', () => {
-  const shipModules = solution.__get__('ship').modules;
-  const availableModules = solution.__get__('availableModules');
+  const shipModules = code.__get__('ship').modules;
+  const availableModules = code.__get__('availableModules');
   const lifeSupportModule = availableModules[1];
   expect(shipModules[0]).toEqual(lifeSupportModule);
 });
 
 // Challenge #5
 test('Challenge #5: `findModuleIndex` function', () => {
-  expect(solution.__get__('findModuleIndex')('propulsion')).toBe(2);
+  expect(code.__get__('findModuleIndex')('propulsion')).toBe(2);
 });
 
 test('Challenge #5: `propulsion` module added to shipModules', () => {
-  const shipModules = solution.__get__('ship').modules;
-  const availableModules = solution.__get__('availableModules');
+  const shipModules = code.__get__('ship').modules;
+  const availableModules = code.__get__('availableModules');
   const propulsionModule = availableModules[2];
   expect(shipModules[1]).toEqual(propulsionModule);
 });
 
 // Challenge #6
 test('Challenge #6: `navigation` module added to shipModules', () => {
-  const shipModules = solution.__get__('ship').modules;
-  const availableModules = solution.__get__('availableModules');
+  const shipModules = code.__get__('ship').modules;
+  const availableModules = code.__get__('availableModules');
   const navigationModule = availableModules[3];
   expect(shipModules[2]).toEqual(navigationModule);
 });
