997117c4365194d06be377049e0f63fcc4e05af2
Matsuko Friedland <info@matsuko.ca>
2019-11-01 10:48:12 -0400

    Add day 1 solution

---
+++ 2019-11/solutions.js
@@ -0,0 +1,5 @@
+const doorToDoor = (volunteers, neighbourhoods) => {
+  const numVolunteers = volunteers.length;
+const numNeighbourhoods = neighbourhoods.length;
+return numNeighbourhoods / numVolunteers;
+}
