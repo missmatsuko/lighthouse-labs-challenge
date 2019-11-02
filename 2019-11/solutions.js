d9f060ab2d5c46c7db7579d4d91469db80c69246
Matsuko Friedland <info@matsuko.ca>
2019-11-02 13:19:41 -0400

    Add day 2 solution

--- 2019-11/solutions.js
+++ 2019-11/solutions.js
@@ -3,3 +3,19 @@ const doorToDoor = (volunteers, neighbourhoods) => {
 const numNeighbourhoods = neighbourhoods.length;
 return numNeighbourhoods / numVolunteers;
 }
+
+const interviewAnswer = (topic) => {
+  switch(topic) {
+    case 'arts funding':
+      return "We'll have to get creative!"
+      break;
+    case 'economy':
+      return "Time is money."
+      break;
+    case 'transportation':
+      return "It's going to be a long road, so we better get moving."
+      break;
+    default:
+      return "QUACK!";
+  }
+}
