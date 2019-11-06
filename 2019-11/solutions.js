b367280d9b966fe8d0be77c15f4f76ecddf37d83
Matsuko Friedland <info@matsuko.ca>
2019-11-05 16:07:59 -0800

    Add day 5

--- 2019-11/solutions.js
+++ 2019-11/solutions.js
@@ -33,3 +33,15 @@ const castVote = (name, votes) => {
 const registerToVote = (name, unregisteredVoters) => {
   return unregisteredVoters.filter(voter => voter !== name);
 }
+
+const chooseStations = (stations) => {
+  return stations
+    .filter(station => {
+      const capacity = station[1];
+      const type = station[2];
+
+      return capacity >=20 && ['school', 'community centre'].includes(type);
+    })
+    .map(station => station[0])
+  ;
+}
